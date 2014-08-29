5e41151e8f64cb366d405db0183c4ac4791ef9fe
Ryo Nakamura <r7kamura@gmail.com>
2014-08-29 11:02:12 +0900

    Add the 1st implementation

--- lib/rspec/request_describer.rb
+++ lib/rspec/request_describer.rb
@@ -2,6 +2,53 @@ require "rspec/request_describer/version"
 
 module RSpec
   module RequestDescriber
-    # Your code goes here...
+    RESERVED_HEADER_NAMES = %w[
+      Content-Type
+      Host
+    ]
+
+    def self.included(base)
+      base.instance_eval do
+        subject do
+          send method, path, request_body, env
+        end
+
+        let(:request_body) do
+          if headers['Content-Type'] == 'application/json'
+            params.to_json
+          else
+            params
+          end
+        end
+
+        let(:headers) do
+          {}
+        end
+
+        let(:params) do
+          {}
+        end
+
+        let(:env) do
+          headers.inject({}) do |result, (key, value)|
+            key = 'HTTP_' + key unless key.in?(RESERVED_HEADER_NAMES)
+            key = key.gsub('-', '_').upcase
+            result.merge(key => value)
+          end
+        end
+
+        let(:endpoint_segments) do
+          example.full_description.match(/(GET|POST|PUT|PATCH|DELETE) ([\/a-z0-9_:]+)/).to_a
+        end
+
+        let(:method) do
+          endpoint_segments[1].downcase
+        end
+
+        let(:path) do
+          endpoint_segments[2].gsub(/:([^\s\/]+)/) { send($1) }
+        end
+      end
+    end
   end
 end
