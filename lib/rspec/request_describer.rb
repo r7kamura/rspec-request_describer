8b6f79bb87903a77e82f4b4597591f5720372b78
r7kamura <r7kamura@gmail.com>
2017-02-27 14:50:17 +0900

    Use class << self instead of def self.xxx

--- lib/rspec/request_describer.rb
+++ lib/rspec/request_describer.rb
@@ -16,65 +16,67 @@ module RSpec
       DELETE
     )
 
-    def self.included(base)
-      base.instance_eval do
-        subject do
-          send_request
-        end
-
-        if ::RSpec::RequestDescriber.process_with_kwargs?
-          let(:send_request) do
-            send http_method, path, params: request_body, headers: env
+    class << self
+      def included(base)
+        base.instance_eval do
+          subject do
+            send_request
           end
-        else
-          let(:send_request) do
-            send http_method, path, request_body, env
-          end
-        end
 
-        let(:request_body) do
-          if headers.any? { |key, value| key.downcase == "content-type" && value == "application/json" }
-            params.to_json
+          if ::RSpec::RequestDescriber.process_with_kwargs?
+            let(:send_request) do
+              send http_method, path, params: request_body, headers: env
+            end
           else
-            params
+            let(:send_request) do
+              send http_method, path, request_body, env
+            end
           end
-        end
 
-        let(:headers) do
-          {}
-        end
-
-        let(:params) do
-          {}
-        end
-
-        let(:env) do
-          headers.inject({}) do |result, (key, value)|
-            key = "HTTP_" + key unless RESERVED_HEADER_NAMES.include?(key)
-            key = key.gsub("-", "_").upcase
-            result.merge(key => value)
+          let(:request_body) do
+            if headers.any? { |key, value| key.downcase == "content-type" && value == "application/json" }
+              params.to_json
+            else
+              params
+            end
           end
-        end
 
-        let(:endpoint_segments) do
-          current_example = RSpec.respond_to?(:current_example) ? RSpec.current_example : example
-          current_example.full_description.match(/(#{SUPPORTED_METHODS.join("|")}) (\S+)/).to_a
-        end
+          let(:headers) do
+            {}
+          end
 
-        let(:http_method) do
-          endpoint_segments[1].downcase
-        end
+          let(:params) do
+            {}
+          end
 
-        let(:path) do
-          endpoint_segments[2].gsub(/:(\w+[!?=]?)/) { send($1) }
+          let(:env) do
+            headers.inject({}) do |result, (key, value)|
+              key = "HTTP_" + key unless RESERVED_HEADER_NAMES.include?(key)
+              key = key.gsub("-", "_").upcase
+              result.merge(key => value)
+            end
+          end
+
+          let(:endpoint_segments) do
+            current_example = RSpec.respond_to?(:current_example) ? RSpec.current_example : example
+            current_example.full_description.match(/(#{SUPPORTED_METHODS.join("|")}) (\S+)/).to_a
+          end
+
+          let(:http_method) do
+            endpoint_segments[1].downcase
+          end
+
+          let(:path) do
+            endpoint_segments[2].gsub(/:(\w+[!?=]?)/) { send($1) }
+          end
         end
       end
-    end
 
-    # @private
-    def self.process_with_kwargs?
-      ActionDispatch::Integration::Session.instance_method(:process) ||
-      ActionDispatch::Integration::Session.instance_method(:process_with_kwargs) rescue false
+      # @private
+      def process_with_kwargs?
+        ActionDispatch::Integration::Session.instance_method(:process) ||
+        ActionDispatch::Integration::Session.instance_method(:process_with_kwargs) rescue false
+      end
     end
   end
 end
