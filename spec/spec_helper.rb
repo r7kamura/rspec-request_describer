7fb12deb331fe662dc87bb11b3f176170c25d217
r7kamura <r7kamura@gmail.com>
2018-12-11 00:36:02 +0900

    Auto-correct by RuboCop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,5 @@ RSpec.configure do |config|
 
   config.warnings = true
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 end
