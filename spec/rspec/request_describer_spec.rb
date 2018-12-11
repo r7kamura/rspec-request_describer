e36a776ef078a0878f520bd3be93d157dc57b7f5
r7kamura <r7kamura@gmail.com>
2018-12-11 16:50:24 +0900

    Add tests

---
+++ spec/rspec/request_describer_spec.rb
@@ -0,0 +1,88 @@
+RSpec.describe RSpec::RequestDescriber do
+  include RSpec::RequestDescriber
+
+  def get(*args)
+    [__method__, *args]
+  end
+
+  describe 'GET /users' do
+    it 'calls #get' do
+      is_expected.to eq(
+        [
+          :get,
+          '/users',
+          headers: {},
+          params: {}
+        ]
+      )
+    end
+
+    context 'with headers' do
+      let(:headers) do
+        super().merge('Authorization' => 'token 12345')
+      end
+
+      it 'calls #get with HTTP_ prefixed and upper-cased headers' do
+        is_expected.to eq(
+          [
+            :get,
+            '/users',
+            headers: { 'HTTP_AUTHORIZATION' => 'token 12345' },
+            params: {}
+          ]
+        )
+      end
+    end
+
+    context 'with headers including reserved header name' do
+      let(:headers) do
+        super().merge('Https' => 'on')
+      end
+
+      it 'calls #get with headers with non HTTP_ prefixed and upper-cased headers' do
+        is_expected.to eq(
+          [
+            :get,
+            '/users',
+            headers: { 'HTTPS' => 'on' },
+            params: {}
+          ]
+        )
+      end
+    end
+
+    context 'with params' do
+      let(:params) do
+        super().merge('sort' => 'id')
+      end
+
+      it 'calls #get with params' do
+        is_expected.to eq(
+          [
+            :get,
+            '/users',
+            headers: {},
+            params: { 'sort' => 'id' }
+          ]
+        )
+      end
+    end
+  end
+
+  describe 'GET /users/:user_id' do
+    let(:user_id) do
+      1
+    end
+
+    it 'calles #get with embeded variable in URL path' do
+      is_expected.to eq(
+        [
+          :get,
+          '/users/1',
+          headers: {},
+          params: {}
+        ]
+      )
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'rspec/request_describer'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
